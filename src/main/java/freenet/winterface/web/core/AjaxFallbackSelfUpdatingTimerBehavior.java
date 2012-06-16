db67830519f9ff825f2889ee0611cad91ee9b9b6
pausb <pausb@quaintous.com>
2012-06-16 18:14:28 +0200

    Fixed AjaxFallbackSelfUpdatubgTimerBehavior JS detection problem with a simple <noscript> tag!

--- src/main/java/freenet/winterface/web/core/AjaxFallbackSelfUpdatingTimerBehavior.java
+++ src/main/java/freenet/winterface/web/core/AjaxFallbackSelfUpdatingTimerBehavior.java
@@ -5,7 +5,6 @@ import org.apache.wicket.Page;
 import org.apache.wicket.ajax.AjaxSelfUpdatingTimerBehavior;
 import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.markup.html.IHeaderResponse;
-import org.apache.wicket.protocol.http.request.WebClientInfo;
 import org.apache.wicket.util.time.Duration;
 
 /**
@@ -14,11 +13,6 @@ import org.apache.wicket.util.time.Duration;
  * {@link Component}s corresponding {@link Page}, which causes the page to
  * refresh regarding the desired {@link Duration}
  * <p>
- * Please note that JS detection is done by utilizing {@link WebClientInfo},
- * which collects data only <b>once</b> per session. So if user turns off the
- * JavaScript in middle of the session, this method return faulty results (see source of 
- * {@link AjaxFallbackSelfUpdatingTimerBehavior#isJSEnabled()}).
- * </p>
  * 
  * @author pausb
  * 
@@ -38,21 +32,8 @@ public class AjaxFallbackSelfUpdatingTimerBehavior extends AjaxSelfUpdatingTimer
 
 	@Override
 	public void renderHead(Component component, IHeaderResponse response) {
-		if (isJSEnabled()) {
-			super.renderHead(component, response);
-		} else {
-			response.renderString("<meta http-equiv=\"refresh\" content=\"" + getUpdateInterval().seconds() + "\">");
-		}
-	}
-
-	/**
-	 * Checks if JavaScript is enable.
-	 * 
-	 * @return {@code false} if JavaScript is not available.
-	 */
-	protected boolean isJSEnabled() {
-		WebClientInfo clientInfo = (WebClientInfo) getComponent().getSession().getClientInfo();
-		return clientInfo.getProperties().isJavaEnabled();
+		super.renderHead(component, response);
+		response.renderString("<noscript><meta http-equiv=\"refresh\" content=\"" + getUpdateInterval().seconds() + "\"></noscript>");
 	}
 
 }
