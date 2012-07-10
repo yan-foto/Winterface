b38c53faae6c9c86ae91ddfa6a85d60c18fbf284
pausb <pausb@quaintous.com>
2012-07-10 22:46:43 +0200

    ServerManager now prepares server for using Atmosphere with websocket support.

--- src/main/java/freenet/winterface/core/ServerManager.java
+++ src/main/java/freenet/winterface/core/ServerManager.java
@@ -8,9 +8,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 import org.apache.wicket.protocol.http.ContextParamWebApplicationFactory;
 import org.apache.wicket.protocol.http.WicketFilter;
-import org.apache.wicket.protocol.http.WicketServlet;
+import org.atmosphere.cpr.ApplicationConfig;
+import org.atmosphere.cpr.MeteorServlet;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.bio.SocketConnector;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ErrorPageErrorHandler;
 import org.eclipse.jetty.servlet.FilterHolder;
@@ -70,7 +71,7 @@ public class ServerManager {
 			// Bind
 			String[] hosts = Configuration.getBindToHosts().split(",");
 			for (String host : hosts) {
-				SocketConnector connector = new SocketConnector();
+				SelectChannelConnector connector = new SelectChannelConnector();
 				connector.setMaxIdleTime(Configuration.getIdleTimeout());
 				connector.setSoLingerTime(-1);
 				connector.setHost(host);
@@ -79,22 +80,34 @@ public class ServerManager {
 			}
 
 			ServletContextHandler sch = new ServletContextHandler(ServletContextHandler.SESSIONS);
+			ServletHolder meteorServletHolder = new ServletHolder(MeteorServlet.class);
+			meteorServletHolder.setInitParameter(ApplicationConfig.FILTER_CLASS, WicketFilter.class.getName());
+			meteorServletHolder.setInitParameter(ApplicationConfig.WEBSOCKET_SUPPORT, "true");
+			meteorServletHolder.setInitParameter(ApplicationConfig.PROPERTY_NATIVE_COMETSUPPORT, "true");
+
+			// XXX Evil dirty hack
+			// This is necessary so Atmosphere passes URLs containing "@" down
+			// the filter chain
+			// This is supposed to be fixed in future releases (see
+			// https://github.com/Atmosphere/atmosphere/issues/498)
+			meteorServletHolder.setInitParameter(ApplicationConfig.MAPPING, "[a-zA-Z0-9-&.=;\\,\\\\~?@]+");
+
+			meteorServletHolder.setInitParameter(ContextParamWebApplicationFactory.APP_CLASS_PARAM, WinterfaceApplication.class.getName());
+			meteorServletHolder.setInitParameter(WicketFilter.FILTER_MAPPING_PARAM, "/*");
+			if (!devMode) {
+				meteorServletHolder.setInitParameter("wicket.configuration", "deployment");
+			}
+			meteorServletHolder.setInitOrder(0);
 			FilterHolder fh = new FilterHolder(IPFilter.class);
 			fh.setInitParameter(IPFilter.ALLOWED_HOSTS_PARAM, Configuration.getAllowedHosts());
 			sch.addFilter(fh, "/*", EnumSet.of(DispatcherType.REQUEST));
-			ServletHolder sh = new ServletHolder(WicketServlet.class);
-			sh.setInitParameter(ContextParamWebApplicationFactory.APP_CLASS_PARAM, WinterfaceApplication.class.getName());
-			sh.setInitParameter(WicketFilter.FILTER_MAPPING_PARAM, "/*");
-			if (!devMode) {
-				sh.setInitParameter("wicket.configuration", "deployment");
-			}
 
 			ErrorPageErrorHandler errorHandler = new ErrorPageErrorHandler();
 			errorHandler.addErrorPage(HttpServletResponse.SC_NOT_FOUND, "/error");
 			errorHandler.addErrorPage(HttpServletResponse.SC_FORBIDDEN, "/error");
 			sch.setErrorHandler(errorHandler);
 
-			sch.addServlet(sh, "/*");
+			sch.addServlet(meteorServletHolder, "/*");
 
 			// Static resources
 			String staticPath = WinterfacePlugin.class.getClassLoader().getResource("static/").toExternalForm();
@@ -119,6 +132,7 @@ public class ServerManager {
 			try {
 				logger.info("Starting Jetty Server on port " + Configuration.getPort());
 				server.start();
+				server.join();
 			} catch (Exception e) {
 				logger.error("Error by server startup!", e);
 			}
