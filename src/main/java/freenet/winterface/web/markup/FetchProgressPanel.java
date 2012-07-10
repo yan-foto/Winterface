69a10f89c7e5969a0cae10cbe95d10675afc5ceb
pausb <pausb@quaintous.com>
2012-07-10 22:51:44 +0200

    A Panel to show progress of fetch process (used in FreenetURIPage)

---
+++ src/main/java/freenet/winterface/web/markup/FetchProgressPanel.java
@@ -0,0 +1,32 @@
+package freenet.winterface.web.markup;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+
+@SuppressWarnings("serial")
+public class FetchProgressPanel extends Panel {
+
+	IModel<Float> progressModel;
+
+	public FetchProgressPanel(String id, IModel<Float> model) {
+		super(id);
+		this.progressModel = model;
+	}
+
+	@Override
+	protected void onInitialize() {
+		super.onInitialize();
+		StringBuffer buffer = new StringBuffer("<progress");
+		if (!(progressModel.getObject() <= 0)) {
+			buffer.append(" value=");
+			buffer.append(progressModel.getObject());
+		}
+		buffer.append("></progress>");
+		Label progress = new Label("progress",Model.of(buffer.toString()));
+		progress.setEscapeModelStrings(false);
+		add(progress);
+	}
+
+}
