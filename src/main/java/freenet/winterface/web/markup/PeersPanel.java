9d369ecfaba44e8839a3458d31eaf3c878805e86
pausb <pausb@quaintous.com>
2012-08-05 14:10:26 +0200

    Refactored PeersPanel: * Moved adding components from constructor to initializer * Fixed broken self updating behavior using a proper model Refactored Dashboard: * Made necessary changes to adapt new PeersPanel

--- src/main/java/freenet/winterface/web/markup/PeersPanel.java
+++ src/main/java/freenet/winterface/web/markup/PeersPanel.java
@@ -4,12 +4,10 @@ import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxSelfUpdatingTimerBehavior;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.model.Model;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.util.time.Duration;
 
 import freenet.node.PeerManager;
-import freenet.winterface.core.FreenetWrapper;
-import freenet.winterface.web.core.WinterfaceApplication;
 
 /**
  * {@link DashboardPanel} which shows sumarized information about peers.
@@ -24,31 +22,35 @@ public class PeersPanel extends DashboardPanel {
 	 * 
 	 * @param id
 	 *            {@link Component} markup ID
+	 * @param model
+	 *            {@link IModel} to retrieve data from
 	 */
-	public PeersPanel(String id) {
-		super(id);
-		WinterfaceApplication app = (WinterfaceApplication) getApplication();
-		FreenetWrapper freenetWrapper = app.getFreenetWrapper();
-		PeerManager peers = freenetWrapper.getNode().peers;
+	public PeersPanel(String id, IModel<PeerManager> model) {
+		super(id, model);
+	}
+
+	@Override
+	protected void onInitialize() {
+		super.onInitialize();
 		// Container
 		WebMarkupContainer container = new WebMarkupContainer("peers-stat");
 		container.setOutputMarkupId(true); // Needed for Ajax auto refresh
 		container.add(new AjaxSelfUpdatingTimerBehavior(Duration.seconds(30)));
 		add(container);
 		// All peers
-		Label allPeers = new Label("peers-count", Model.of(peers.countConnectedPeers()));
+		Label allPeers = new Label("countConnectedPeers");
 		container.add(allPeers);
 		// Darknet Peers
-		Label darknetPeers = new Label("darknet-count", Model.of(peers.countConnectedDarknetPeers()));
+		Label darknetPeers = new Label("countConnectedDarknetPeers");
 		container.add(darknetPeers);
 		// Opennet Peers
-		Label opennetPeers = new Label("opennet-count", Model.of(peers.countConnectedDarknetPeers()));
+		Label opennetPeers = new Label("countConnectedOpennetPeers");
 		container.add(opennetPeers);
 		// Backed off peers
-		Label backedOff = new Label("backed-count", Model.of(peers.countBackedOffPeers(true)));
+		Label backedOff = new Label("countBackedOffPeersEither");
 		container.add(backedOff);
 		// Seed Nodes
-		Label seedCount = new Label("seed-count", Model.of(peers.countSeednodes()));
+		Label seedCount = new Label("countSeednodes");
 		container.add(seedCount);
 	}
 
