2c284f8f9effbd7620cc0e35c67a28511b5f4823
pausb <pausb@quaintous.com>
2012-08-14 16:47:12 +0200

    Added VisitURIPanel to dashboard (home page)

--- src/main/java/freenet/winterface/web/Dashboard.java
+++ src/main/java/freenet/winterface/web/Dashboard.java
@@ -8,6 +8,7 @@ import freenet.winterface.web.core.WinterfaceApplication;
 import freenet.winterface.web.markup.BookmarksPanel;
 import freenet.winterface.web.markup.PeersPanel;
 import freenet.winterface.web.markup.VersionPanel;
+import freenet.winterface.web.markup.VisitURIPanel;
 
 
 /**
@@ -32,6 +33,7 @@ public class Dashboard extends WinterPage {
 		add(new BookmarksPanel("bookmarks-panel"));
 		add(new PeersPanel("peers-panel",new CompoundPropertyModel<PeerManager>(peerManagerModel)));
 		add(new VersionPanel("version"));
+		add(new VisitURIPanel("fetchKey"));
 	}
 	
 }
